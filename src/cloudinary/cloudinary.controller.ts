e40e085c8255448e1572dce3723557bb4f541cde
SILAMEAS <sila.meas@allweb.com.kh>
2024-08-26 16:14:35 +0700

    remove-spec-and-app-upload-to-swagger

--- src/cloudinary/cloudinary.controller.ts
+++ src/cloudinary/cloudinary.controller.ts
@@ -1,18 +1,28 @@
-import { Controller, Get, Param, Post, UploadedFile, UseInterceptors } from "@nestjs/common";
+import { Controller, Post, UploadedFile, UseInterceptors } from "@nestjs/common";
 import { FileInterceptor } from "@nestjs/platform-express";
 import { CloudinaryService } from "./cloudinary.service";
-
+import { ApiBearerAuth, ApiBody, ApiConsumes, ApiTags } from "@nestjs/swagger";
+@ApiTags('upload with cloudinary')
+@ApiBearerAuth('access-token')
 @Controller('cloudinary')
 export class CloudinaryController {
   constructor(private readonly cloudinaryService: CloudinaryService) {
   }
-  @Get()
-  sd(){
-    return "JSKLDfj"
-  }
   @Post("/upload")
   @UseInterceptors(FileInterceptor('image') as any)
-  async uploadImage(@Param() id:string, @UploadedFile() image: Express.Multer.File) {
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        image: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  async uploadImage(@UploadedFile() image: Express.Multer.File) {
     try {
       return await this.cloudinaryService.uploadImage(image)
     }catch (e){
